58414c754294b5e40ff2afff0ab878d86c499162
EdgarLobo235 <evillalobos70518@ufide.ac.cr>
2025-07-25 23:08:52 -0600

    Catalogo Ed

---
+++ Proyecto.UI/Controllers/CarritoController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+public class CarritoController : Controller
+{
+    
+    [HttpPost]
+    public IActionResult Agregar(int idProducto)
+    {
+        
+        TempData["Mensaje"] = $"Producto {idProducto} agregado al carrito";
+
+        
+        return RedirectToAction("Catalogo", "Productos");
+    }
+}
+
